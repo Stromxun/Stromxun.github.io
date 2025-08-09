da33e9ca50402a1071c3b109766da3959bd7983b
Stromxun <1512144613@qq.com>
2025-08-09 10:39:36 +0800

    Site updated: 2025-08-09 10:39:36

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/07/15/LeetCode/两数之和/","2025/07/17/算法4/《算法4》-第二章-排序/"];function toRandomPost(){
+var posts=["2025/07/25/算法4/《算法4》-习题2.1/","2025/07/17/算法4/《算法4》-第二章-排序/","2025/07/15/LeetCode/两数之和/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
